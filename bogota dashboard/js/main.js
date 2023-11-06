b84a237da3375d5ad9c8597d11a5e3398e81906c
Ke Zhou for 509 <125450176+acreshectares@users.noreply.github.com>
2023-11-07 03:45:51 +0800

    Add files via upload

---
+++ bogota dashboard/js/main.js
@@ -0,0 +1,15 @@
+import { initializeMap } from './park-map.js';
+import { initializeList } from './park-list.js';
+import { initializeAddressEntry } from './address-entry.js';
+import { setupFilterEvents } from './park-filter.js';
+
+
+const parksResp = await fetch ("data/parks.json");
+const parks = await parksResp.json();
+
+const events = new EventTarget();
+
+initializeMap(parks.features,events);
+initializeList(parks.features,events);
+initializeAddressEntry(parks);
+setupFilterEvents(parks.features, events);
